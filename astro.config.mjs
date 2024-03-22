92b74e3d761dad2a6f5e25b9e695857138ea2523
Ezequiel Menor <ezequielmenor@Mac-mini-de-R.local>
2024-03-22 10:51:25 +0100

    Configuración tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/EzequielMenor/Pagina-Astro.git',
-    base: '/'
+  site: 'https://github.com/EzequielMenor/Pagina-Astro.git',
+  base: '/',
+  integrations: [tailwind()]
 });
