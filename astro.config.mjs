9353b96dd13b5935dd68a6e48ba6d52208eb897d
Ezequiel Menor <ezequielmenor@Mac-mini-de-R.local>
2024-03-22 09:58:54 +0100

    primer commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://github.com/EzequielMenor/Pagina-Astro.git',
+    base: '/'
+});
